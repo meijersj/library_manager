bd5b3c4783112bf10db6b54818a3f70005c68248
meijersj <138444572+meijersj@users.noreply.github.com>
2023-07-03 09:33:30 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,10 @@
+# List of book titles and authors
+book_list = [("To Kill a Mockingbird", "Harper Lee"),
+             ("1984", "George Orwell"),
+             ("The Great Gatsby", "F. Scott Fitzgerald"),
+             ("The Catcher in the Rye", "J.D. Salinger"),
+             ("Pride and Prejudice", "Jane Austen"),
+             ("Heart of Darkness", "Joseph Conrad")]
+
+# List of member names
+member_names = ["Alice", "Bob", "Charlie", "Dave", "Eve"]
---
+++ test.py
@@ -0,0 +1,32 @@
+from main import Library, Book, Member
+from data import book_list, member_names
+
+# Initialize a Library
+lib = Library()
+
+# Add books to the library
+for title, author in book_list:
+    lib.add_book(Book(title, author))
+
+# Register members in the library
+for name in member_names:
+    lib.register_member(Member(name))
+
+# Members borrow books
+lib.borrow_book("Alice", "1984")
+lib.borrow_book("Bob", "The Great Gatsby")
+lib.borrow_book("Charlie", "Pride and Prejudice")
+
+# Display list of all available books
+print("Available Books:")
+for book in lib.get_available_books():
+    print(f"{book.title} by {book.author}")
+
+# Members return books
+lib.return_book("Alice", "1984")
+lib.return_book("Bob", "The Great Gatsby")
+
+# Display list of all available books after some books are returned
+print("\nAvailable Books after some returns:")
+for book in lib.get_available_books():
+    print(f"{book.title} by {book.author}")
